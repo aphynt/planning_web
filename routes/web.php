6c8367ee2c34575467073046d7ad8707d38281fd
aphynt <ahmadfadillah502@gmail.com>
2026-01-04 17:01:29 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return redirect()->route('login');
