d8ad0769a037a389e89e0cae40ebd84533669a65
aphynt <ahmadfadillah502@gmail.com>
2026-08-03 08:40:43 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        URL::forceScheme('https');
+        
         Event::listen(\Illuminate\Console\Events\CommandStarting::class, function ($event) {
             if (in_array($event->command, [
                 'migrate',
