ddc0ad62c5cc7ddd86188721d4d1ceded0989c0f
aphynt <ahmadfadillah502@gmail.com>
2026-08-18 09:38:24 +0800

    update

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,39 +10,127 @@
      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
      <meta name="csrf-token" content="{{ csrf_token() }}">
 
+     @php
+         /*
+         |--------------------------------------------------------------------------
+         | Asset Versioning
+         |--------------------------------------------------------------------------
+         | Menggunakan timestamp file agar browser dan Cloudflare
+         | otomatis mengambil file terbaru ketika CSS berubah.
+         */
+         $appCssVersion = file_exists(public_path('app/assets/css/app.min.css'))
+             ? filemtime(public_path('app/assets/css/app.min.css'))
+             : time();
+
+         $customCssVersion = file_exists(public_path('app/assets/css/custom.css'))
+             ? filemtime(public_path('app/assets/css/custom.css'))
+             : time();
+
+         $vendorCssVersion = file_exists(public_path('app/assets/css/vendor.min.css'))
+             ? filemtime(public_path('app/assets/css/vendor.min.css'))
+             : time();
+
+         $iconsCssVersion = file_exists(public_path('app/assets/css/icons.min.css'))
+             ? filemtime(public_path('app/assets/css/icons.min.css'))
+             : time();
+
+         $fontsCssVersion = file_exists(public_path('app/assets/css/fonts.css'))
+             ? filemtime(public_path('app/assets/css/fonts.css'))
+             : time();
+
+         $configJsVersion = file_exists(public_path('app/assets/js/config.min.js'))
+             ? filemtime(public_path('app/assets/js/config.min.js'))
+             : time();
+     @endphp
+
      <!-- App favicon -->
      <link rel="shortcut icon" href="{{ asset('app') }}/assets/images/sims2.png">
 
      <!-- Gridjs Plugin css -->
-    <link href="{{ asset('app') }}/assets/vendor/gridjs/theme/mermaid.min.css" rel="stylesheet" type="text/css" />
-    {{-- Datatables --}}
-    <link rel="stylesheet" href="{{ asset('app') }}/assets/css/plugins/dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="{{ asset('app') }}/assets/css/plugins/buttons.bootstrap5.min.css">
+     <link
+         href="{{ asset('app') }}/assets/vendor/gridjs/theme/mermaid.min.css"
+         rel="stylesheet"
+         type="text/css"
+     />
 
-     <!-- Vendor css (Require in all Page) -->
-     <link href="{{ asset('app') }}/assets/css/vendor.min.css" rel="stylesheet" type="text/css" />
+     {{-- Datatables --}}
+     <link
+         rel="stylesheet"
+         href="{{ asset('app') }}/assets/css/plugins/dataTables.bootstrap5.min.css"
+     >
 
-     <!-- Icons css (Require in all Page) -->
-     <link href="{{ asset('app') }}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+     <link
+         rel="stylesheet"
+         href="{{ asset('app') }}/assets/css/plugins/buttons.bootstrap5.min.css"
+     >
 
-     <!-- App css (Require in all Page) -->
-     <link href="{{ asset('app') }}/assets/css/app.min.css" rel="stylesheet" type="text/css" />
-     <link rel="stylesheet" href="{{ asset('app') }}/assets/css/fonts.css">
+     <!-- Vendor css -->
+     <link
+         href="{{ asset('app') }}/assets/css/vendor.min.css?v={{ $vendorCssVersion }}"
+         rel="stylesheet"
+         type="text/css"
+     />
 
-     <!-- Theme Config js (Require in all Page) -->
-     <script src="{{ asset('app') }}/assets/js/config.min.js"></script>
-     <link href="{{ asset('app') }}/assets/css/custom.css" rel="stylesheet" type="text/css" />
+     <!-- Icons css -->
+     <link
+         href="{{ asset('app') }}/assets/css/icons.min.css?v={{ $iconsCssVersion }}"
+         rel="stylesheet"
+         type="text/css"
+     />
+
+     <!-- App css -->
+     <!-- Otomatis berubah URL jika app.min.css berubah -->
+     <link
+         href="{{ asset('app') }}/assets/css/app.min.css?v={{ $appCssVersion }}"
+         rel="stylesheet"
+         type="text/css"
+     />
+
+     <!-- Fonts css -->
+     <link
+         rel="stylesheet"
+         href="{{ asset('app') }}/assets/css/fonts.css?v={{ $fontsCssVersion }}"
+     >
+
+     <!-- Theme Config js -->
+     <script
+         src="{{ asset('app') }}/assets/js/config.min.js?v={{ $configJsVersion }}"
+     ></script>
+
+     <!-- Custom css -->
+     <!-- Otomatis berubah URL jika custom.css berubah -->
+     <link
+         href="{{ asset('app') }}/assets/css/custom.css?v={{ $customCssVersion }}"
+         rel="stylesheet"
+         type="text/css"
+     />
 </head>
 
 <body>
+
     @include('layout.alert')
+
     <style>
         @keyframes shake {
-            0% { transform: rotate(0deg); }
-            25% { transform: rotate(5deg); }
-            50% { transform: rotate(-5deg); }
-            75% { transform: rotate(5deg); }
-            100% { transform: rotate(0deg); }
+            0% {
+                transform: rotate(0deg);
+            }
+
+            25% {
+                transform: rotate(5deg);
+            }
+
+            50% {
+                transform: rotate(-5deg);
+            }
+
+            75% {
+                transform: rotate(5deg);
+            }
+
+            100% {
+                transform: rotate(0deg);
+            }
         }
 
         .shake-ikon {
@@ -54,7 +141,6 @@
         .table-wrapper {
             position: relative;
         }
-
 
         #loadingOverlay {
             position: absolute;
@@ -81,11 +167,10 @@
         }
 
         div.dataTables_processing,
-        div.dt-processing{
-            display:none !important;
+        div.dt-processing {
+            display: none !important;
         }
     </style>
 
-
-     <!-- START Wrapper -->
-     <div class="wrapper">
+    <!-- START Wrapper -->
+    <div class="wrapper">
